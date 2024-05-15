ec9fec0a505386d02e3e7a95e108e21745f0090d
webgit12 <webstudent80@gmail.com>
2024-05-15 13:55:36 +0530

    Upload Status

---
+++ src/App.js
@@ -0,0 +1,28 @@
+function App()
+{
+    return(
+        <>
+        <h1>App</h1>
+        <table>
+            <tr>
+                <th>S.No.</th>
+                <th>Name</th>
+            </tr>
+            <tr>
+                <td>1
+
+                </td>
+                <td>Nida</td>
+            </tr>
+            <tr>
+                <td>2
+
+                </td>
+                <td>Aisha</td>
+            </tr>
+            
+        </table>
+        </>
+    )
+}
+export default App
